edd96a2d4dcbbb6aa7fb4137a10fc8667880d91f
Pablo Bascuñana <pablo.bascunana@innocv.com>
2024-05-03 16:23:17 +0200

    Add modal component and modal stories

---
+++ src/stories/Modal.stories.ts
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import { computed } from 'vue';
+import Modal from './Modal.vue';
+import Button from './Button.vue';
+import { Primary } from './Button.stories';
+
+const meta = {
+  title: 'Example/Modal',
+  component: Modal,
+  subcomponents: { Button },
+  tags: ['autodocs'],
+  args: {
+    visible: false
+  },
+  argTypes: {
+    visible:{
+      control: { type: 'boolean' },
+    }
+  }
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const modal: Story = {
+  args: {
+    visible: true,
+  },
+}
+
+export const modalVisibility: Story = {
+  render: (args) => ({
+    components: { Modal, Button },
+    template: `
+      <button type="button" :class="classes" @click="onClick" :style="style">{{ buttonLabel }}</button>
+      <modal :visible="args.visible" @close="closeModal"></modal>
+    `,
+    setup() {
+      const buttonLabel = computed(() => Primary.args.label)
+      const classes = computed(() => ({
+        'storybook-button': true,
+        'storybook-button--primary': Primary.args.primary,
+        'storybook-button--secondary': !Primary.args.primary,
+        [`storybook-button--${Primary.args.size}`]: true,
+      }));
+
+      const onClick = () => {
+        args.visible = true;
+      }
+
+      const closeModal = () => {
+        args.visible = false;
+      }
+    return { args, classes, buttonLabel, closeModal, onClick };
+    },
+  }),
+  args: {
+    visible: false,
+  }
+};
+Primary.args = {
+  label: 'Show Modal',
+  primary: true,
+  size: 'large',
+}
---
+++ src/stories/Modal.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="modal-backdrop" v-if="visible">
+    <div class="modal">
+      <header class="modal-header">
+        <slot name="header">
+          This is the default title!
+        </slot>
+        <button
+          type="button"
+          class="btn-close"
+          @click="onClose"
+        >
+          x
+        </button>
+      </header>
+
+      <section class="modal-body">
+        <slot name="body">
+          This is the default body!
+        </slot>
+      </section>
+
+      <footer class="modal-footer">
+        <slot name="footer">
+          This is the default footer!
+        </slot>
+        <button
+          type="button"
+          class="btn-green"
+          @click="onClose"
+        >
+          Close Modal
+        </button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import './modal.css';
+
+withDefaults(defineProps<{
+  /**
+   * If the modal is visible or not
+   */
+  visible: boolean,
+}>(), { visible: false });
+
+const emit = defineEmits<{
+  (e: 'close'): void;
+}>();
+
+const onClose = () => {
+  emit("close");
+};
+
+</script>
